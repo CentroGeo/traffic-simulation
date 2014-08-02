c1fcc220b55610afbb05bd2aff83f7ef9dfc2ff5
plablo <plablo09@gmail.com>
2014-08-01 19:14:58 -0500

    Parsing output to build time averages

--- sumo_utilities/output_parser_script.py
+++ sumo_utilities/output_parser_script.py
@@ -1,66 +1,21 @@
 # -*- coding: utf-8 -*-
 from xml_handlers.parsers.v_type_probe_parser import v_type_probe_parse
 from xml_handlers.parsers.v_type_probe_parser import write_advisor_files
+from sets import Set
 
-write_advisor_files("data/prueba_salida.xml")
-print 'listo!'
-# import xml.sax
-# import csv
-# from models.vehicle import Vehicle
-#
-# parsedVehicles={}
-# #time = 0
-#
-#
-#
-#
-# class OutputVehicleContentHandler(xml.sax.ContentHandler):
-#     """Parsea el xml de salida de SUMO (para vtypeProbe) y construye un
-#         diccionario con los vehículos (instancias de la clase Vehicle) leídos:
-#         parsedVehicles {'id':Vehicle}
-#     """
-#     def __init__(self):
-#         xml.sax.ContentHandler.__init__(self)
-#         self.time = 0
-#
-#     def startElement(self, name, attrs):
-#         if name == 'timestep':
-#             #global time #= int(float(attrs.get('time')))
-#             self.time = int(float(attrs.get('time')))
-#             #print(time)
-#         elif name == 'vehicle':
-#             if attrs.get('id') in parsedVehicles.keys():
-#                 esteVehicle = parsedVehicles[attrs.get('id')]
-#                 esteVehicle.timesteps.append(self.time)
-#                 esteVehicle.speeds.append(attrs.get('speed'))
-#                 esteVehicle.lanes.append(attrs.get('lane'))
-#                 esteVehicle.positions.append(attrs.get('pos'))
-#                 print('nuevo tiempo ' + attrs.get('id'))
-#             else:
-#                 parsedVehicles[attrs.get('id')]=Vehicle(attrs.get('id'),
-#                                                         self.time,
-#                                                         attrs.get('speed'),
-#                                                         attrs.get('lane'),
-#                                                         attrs.get('pos'))
-#                 print('nuevo vehiculo ' + attrs.get('id'))
-#
-#     def endElement(self, name):
-#         #print("endElement '" + name + "'")
-#         pass
-#
-#
-#
-# def main(sourceFileName):
-#     source = open(sourceFileName)
-#     xml.sax.parse(source, OutputVehicleContentHandler())
-#     for k,v in parsedVehicles.items():
-#         with open("output/v_"+k,"wb") as csvfile:
-#             spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|',
-#                                     quoting=csv.QUOTE_MINIMAL)
-#             for j,t in enumerate(v.timesteps):
-#                 renglon = [v.id,t,v.speeds[j]]
-#                 spamwriter.writerow(renglon)
-#
-#
-# if __name__ == "__main__":
-#     main("data/prueba_salida.xml")
+#write_advisor_files("data/prueba_salida.xml")
+parsed_vehicles = v_type_probe_parse("data/prueba_salida.xml")
+timesteps = []
+for k,v in parsed_vehicles.items():
+  timesteps.extend(v.timesteps)
+
+timesteps = list(Set(timesteps))
+
+timsteps_vehicles = {}
+for t in timesteps:
+  timsteps_vehicles[str(t)]=[]
+  for k,v in parsed_vehicles.items():
+    if t in v.timesteps:
+        timsteps_vehicles[str(t)].append(v)
+
+print timsteps_vehicles
